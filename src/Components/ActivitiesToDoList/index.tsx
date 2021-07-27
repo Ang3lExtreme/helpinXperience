69a6d920fbe33a69172ec4d03a5966b3ac931c3f
Frederico Neves <frednevesal@gmail.com>
2021-07-27 16:10:24 +0100

    p

--- src/Components/ActivitiesToDoList/index.tsx
+++ src/Components/ActivitiesToDoList/index.tsx
@@ -18,10 +18,8 @@ async function fetcher(path: string) {
 const token: Token = Cookies.getJSON('token')
 export default function ActivitiesToDoList() {
   const [cursor, setCursor] = useState<string>(null);
-  const [cursor2, setCursor2] = useState<string>(null);
 
   const [endlist, setEndlist] = useState<boolean>(true);
-  const [endlist2, setEndlist2] = useState<boolean>(true);
   const [listativities, setListativities] = useState<listAtivitiesTodoProps>([]);
   let username = window.location.pathname.replace('/', '')
   const config = {
@@ -30,9 +28,9 @@ export default function ActivitiesToDoList() {
       'Content-Type': 'application/json'
     }
   }
-  async function fetchData() {
+  function fetchData() {
     //console.log('segundo fetch')
-      await api.post(`activities/listJoinedActivities/?username=${username}`, cursor, config).then( (response) => {
+       api.post(`activities/listJoinedActivities/?username=${username}`, cursor, config).then( (response) => {
   
         if(response.data.results.length === 0 ){
           setEndlist(false);
@@ -45,35 +43,18 @@ export default function ActivitiesToDoList() {
       setCursor(response.data.cursorString)
         //console.log(cursor);
       });
-
-      await api.post(`activities/listCreatedActivities/?username=${username}`, cursor2, config).then( (response) => {
-        
-        if(response.data.results.length === 0 ){
-          setEndlist2(false);
-          return;
-        }
-        //console.log(response.data.results)
-        setListativities((current) => 
-          current.concat(response.data.results)   
-      )
-      setCursor2(response.data.cursorString)
-        //console.log(cursor);
-      });
     }
 
 
-    useEffect(() => {
-      fetchData();
-    }  
-    , [])
+   
   return (
     <div>
        <h3 style={{ textAlign: 'center' }}>Atividades a fazer</h3>
 
 <InfiniteScroll
-          dataLength={listativities.length * 10} //This is important field to render the next data
+          dataLength={listativities.length * 5} //This is important field to render the next data
           next={fetchData}
-          hasMore={endlist && endlist2}
+          hasMore={endlist}
           loader={<h4 style={{ textAlign: 'center' }}>Loading...</h4>}
           //scrollableTarget="target"
           endMessage={

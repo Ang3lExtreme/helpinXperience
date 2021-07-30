c149a7e4c3c69e1cf3032732f0e34e072548bcef
Frederico Neves <frednevesal@gmail.com>
2021-07-30 14:57:14 +0100

    video fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,7 +33,7 @@ export default function Home() {
         <Sobre />
       </div>
       <div className={styles.video}>
-      <iframe width="560" height="315" src="https://www.youtube.com/embed/dkT1rImAcaQ" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/dkT1rImAcaQ" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe>
       </div>
       <div id="contatos">
         <Contato />
